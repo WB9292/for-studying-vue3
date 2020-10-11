50ad250a7946c0baddacf9a576b279ddfc60816e
zhangwubin <zhangwubin7@jd.com>
2020-10-11 21:33:30 +0800

    添加友盟统计;

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,23 @@
+const isDev = process.env.NODE_ENV === 'development'
+let head = []
+
+if(!isDev) {
+  // 友盟的数据统计
+  head = [['script', {}, `
+    document.write(unescape("%3Cspan id='cnzz_stat_icon_1279339182'%3E%3C/span%3E%3Cscript src='https://s9.cnzz.com/z_stat.php%3Fid%3D1279339182%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
+  `]]
+}
+
 module.exports = {
   title: "Vue3源码解析",
   description: "vue3，vue3源码解析，vue3代码分析",
+  head,
   themeConfig: {
     smoothScroll: true,
     lastUpdated: '上一次更新时间',
     nav: [
       { text: "源码解析", link: "/read/" },
-      { text: "支持", link: "/support/" },
+      { text: "赞助", link: "/support/" },
       { text: "源码", link: "https://github.com/WB9292/vue-next" },
     ],
     sidebar: {
--- docs/index.md
+++ docs/index.md
@@ -4,5 +4,5 @@ heroImage: /vue-logo.png
 tagline: 简单、纯粹、直击源码核心
 actionText: 开始吧 →
 actionLink: /read/index.html
-footer: MIT Licensed | Copyright © 2020-present zhangwubin
+footer: MIT Licensed | Copyright © 2020-present zwb9292
 ---
